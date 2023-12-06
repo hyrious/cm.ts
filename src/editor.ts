1c7f1a12a2ebe18aded18227506d58fcac54f81a
hyrious <hyrious@outlook.com>
2023-12-06 11:43:57 +0800

    remove unused code

--- src/editor.ts
+++
@@ -1,8 +0,0 @@
-import { EditorView, basicSetup } from 'codemirror'
-import { javascript } from '@codemirror/lang-javascript'
-
-export const view = new EditorView({
-  doc: `let a = 1`,
-  extensions: [basicSetup, javascript({ typescript: true })],
-  parent: document.getElementById('editor')!
-})
